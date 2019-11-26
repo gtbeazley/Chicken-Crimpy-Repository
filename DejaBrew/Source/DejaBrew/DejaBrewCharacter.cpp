320bc4ed5f3ae315a948613c423f6f4fc19201b5
gtbeazley <50130971+gtbeazley@users.noreply.github.com>
2019-11-26 16:53:16 +1100

    BugFixes
    No longer crashes when simulating the game

--- DejaBrew/Source/DejaBrew/DejaBrewCharacter.cpp
+++ DejaBrew/Source/DejaBrew/DejaBrewCharacter.cpp
@@ -350,34 +350,37 @@ void ADejaBrewCharacter::Die()
 
 void ADejaBrewCharacter::LoadLastCheckpoint()
 {
-	FString slotName = Cast<ADejaBrewGameMode>(UGameplayStatics::GetGameMode(this))->SlotName;
-	if (UGameplayStatics::DoesSaveGameExist(slotName, 0))
+	ADejaBrewGameMode* GMInstance = Cast<ADejaBrewGameMode>(UGameplayStatics::GetGameMode(this));
+	if (GMInstance)
 	{
-		UDejaBrew_SaveGame* SGInstance = Cast<UDejaBrew_SaveGame>(UGameplayStatics::LoadGameFromSlot(slotName, 0));
+		FString slotName = GMInstance->SlotName;
+		if (UGameplayStatics::DoesSaveGameExist(slotName, 0))
+		{
+			UDejaBrew_SaveGame* SGInstance = Cast<UDejaBrew_SaveGame>(UGameplayStatics::LoadGameFromSlot(slotName, 0));
 
-		//Loading Player Stats
-		m_compressionCharge = m_initialCompressionCharge;
-		SetActorLocation(SGInstance->PlayerLoc);
-		m_curScore = SGInstance->curScore;
+			//Loading Player Stats
+			m_compressionCharge = m_initialCompressionCharge;
+			SetActorLocation(SGInstance->PlayerLoc);
+			m_curScore = SGInstance->curScore;
 
-		//Loading all CoffeeBean States
-		for (int32 i = 0; i < SGInstance->CoffeeBeanRefs.Num(); i++)
-			SGInstance->CoffeeBeanRefs[i]->Collected = SGInstance->BeanCollected[i];
+			//Loading all CoffeeBean States
+			for (int32 i = 0; i < SGInstance->CoffeeBeanRefs.Num(); i++)
+				SGInstance->CoffeeBeanRefs[i]->Collected = SGInstance->BeanCollected[i];
 
-		//Loading all fuel states; 
-		for (int32 i = 0; i < SGInstance->FuelRefs.Num(); i++)
-			SGInstance->FuelRefs[i]->Collected = SGInstance->FuelCollected[i];  
+			//Loading all fuel states; 
+			for (int32 i = 0; i < SGInstance->FuelRefs.Num(); i++)
+				SGInstance->FuelRefs[i]->Collected = SGInstance->FuelCollected[i];
 
-		//Loading all Thorns 
-		for (int32 i = 0; i < SGInstance->ThornRefs.Num(); i++)
-			SGInstance->ThornRefs[i]->SetActorLocation(SGInstance->ThornLoc[i]);
+			//Loading all Thorns 
+			for (int32 i = 0; i < SGInstance->ThornRefs.Num(); i++)
+				SGInstance->ThornRefs[i]->SetActorLocation(SGInstance->ThornLoc[i]);
 
-		//Loading all SlimeEnemys
-		for (int32 i = 0; i < SGInstance->EnemyRefs.Num(); i++)
-			SGInstance->EnemyRefs[i]->SetActorLocation(SGInstance->EnemyLoc[i]);
+			//Loading all SlimeEnemys
+			for (int32 i = 0; i < SGInstance->EnemyRefs.Num(); i++)
+				SGInstance->EnemyRefs[i]->SetActorLocation(SGInstance->EnemyLoc[i]);
 
+		}
 	}
-
 }
 
 void ADejaBrewCharacter::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
